cbace8470e2e7fd58e1f22f25f7cb2c737407556
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2024-12-13 20:55:12 +0100

    nfs-genesha: remove exports file and update nfsd.sh for access configuration

--- containers/nfs-genesha/nfsd.sh
+++ containers/nfs-genesha/nfsd.sh
@@ -146,7 +146,12 @@ EXPORT {
   Pseudo = "/";
   ${ALLOWED_CLIENTS}
   Access_Type = ${READ_ONLY_FLAG};
+  Access "*";
+  Root_Access = "*;
   Squash = No_root_squash;
+  FSAL {
+    Name = VFS;
+  }
 }
 EOF
 
